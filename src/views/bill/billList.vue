a2872a30bac7ebe8a9f609cb67b57095a64456ff
zhaojyv <zhaojyv@yonyou.com>
2020-06-23 17:24:43 +0800

    init

---
+++ src/views/bill/billList.vue
@@ -0,0 +1,16 @@
+<template>
+  <page-header-wrapper>
+    <a-card :body-style="{padding: '24px 32px'}" :bordered="false">
+    </a-card>
+  </page-header-wrapper>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
